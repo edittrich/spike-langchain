e5148a06130b112885242558323fa42958d1b060
Erik Dittrich <erik.dittrich@gmail.com>
2024-05-20 14:20:36 +0200

    Initial

--- ice_breaker.py
+++ ice_breaker.py
@@ -1,21 +1,22 @@
+from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+
 from dotenv import load_dotenv
+
 from langchain.prompts.prompt import PromptTemplate
 from langchain_openai import ChatOpenAI
 
 from third_parties.linkedin import scrape_linkedin_profile
 
 
-if __name__ == "__main__":
-    load_dotenv()
-
-    print("Hello LangChain")
+def ice_break_with(name: str) -> str:
+    linkedin_username = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_username, mock=True)
 
     summary_template = """
-    given the Linkedin {information} about a person I want you to create:
+    given the Linkedin information {information} about a person I want you to create:
     1. A short summary
     2. two interesting facts about them
     """
-
     summary_prompt_template = PromptTemplate(
         input_variables=["information"], template=summary_template
     )
@@ -23,9 +24,13 @@ if __name__ == "__main__":
     llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
 
     chain = summary_prompt_template | llm
-    linkedin_data = scrape_linkedin_profile(
-        linkedin_profile_url="https://www.linkedin.com/in/erikdittrich/", mock=True
-    )
     res = chain.invoke(input={"information": linkedin_data})
 
     print(res)
+
+
+if __name__ == "__main__":
+    load_dotenv()
+
+    print("Ice Breaker Enter")
+    ice_break_with(name="Eden Marco Udemy")
